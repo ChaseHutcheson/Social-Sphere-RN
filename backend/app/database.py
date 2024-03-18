12bb63e1376dfbd54c5509877acc7cc69861515b
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-03-18 14:08:29 -0400

    changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,11 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://main:Chaser57@192.168.1.254:5432/testdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/FastAPI"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
